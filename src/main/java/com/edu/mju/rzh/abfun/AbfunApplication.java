fe6d02bf2155a59e4054423a01cff1296b99b73f
Great-rzh <2506895765@qq.com>
2020-02-28 10:42:49 +0800

    init repo

---
+++ src/main/java/com/edu/mju/rzh/abfun/AbfunApplication.java
@@ -0,0 +1,13 @@
+package com.edu.mju.rzh.abfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbfunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbfunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/mju/rzh/abfun/AbfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.mju.rzh.abfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbfunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
